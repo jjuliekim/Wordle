5983fdbafce0939006c1d73df9c66e79a4ac46ff
jjuliekim <118621382+jjuliekim@users.noreply.github.com>
2023-07-04 16:06:28 -0400

    initial commit and layout design

--- src/main/java/me/julie/wordle/Main.java
+++ src/main/java/me/julie/wordle/Main.java
@@ -6,7 +6,6 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        WordleController controller = new WordleController();
         WordleView view = new WordleView();
         stage.setTitle("Wordle");
         stage.setScene(view.load());
